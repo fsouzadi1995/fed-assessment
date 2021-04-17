ab17509053881cf0615608c79415fbdaa3b4c056
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-04-17 16:04:07 -0300

    test: fix test suites

--- src/app/pages/newsletter/newsletter.component.spec.ts
+++ src/app/pages/newsletter/newsletter.component.spec.ts
@@ -1,14 +1,17 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NewsletterComponent } from './newsletter.component';
 
-describe('EmailFormComponent', () => {
+describe('NewsletterComponent', () => {
   let component: NewsletterComponent;
   let fixture: ComponentFixture<NewsletterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NewsletterComponent],
+      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule],
     }).compileComponents();
   });
 
